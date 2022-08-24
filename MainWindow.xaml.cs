90c265328d9bec4181ff46973cdcf1181760ec5e
Lemuura <kraahlix@gmail.com>
2022-08-24 03:13:56 +0200

    Added Select All checkbox Added Any% preset Made changes to minigames used for 100%

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -126,154 +126,295 @@ namespace ITTSGM
         {
             if (whack.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Whack-A-Cody\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Whack-A-Cody_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Whack-A-Cody\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Whack-A-Cody_HaveDiscovered\": \"True\",");
             }
 
             if (flip.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.The Nail Wheel\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.The Nail Wheel_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.The Nail Wheel\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.The Nail Wheel_HaveDiscovered\": \"True\",");
             }
 
             if (tug.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Tug of War\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Tug of War_HaveDiscovered\": \"True\",");
-                saveLinesList.Add("	\"MinigameFlag.Tug of War_HavePlayed\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Tug of War\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Tug of War_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"MinigameFlag.Tug of War_HavePlayed\": \"True\",");
             }
 
             if (plunger.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Plunger\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Plunger_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Plunger\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Plunger_HaveDiscovered\": \"True\",");
             }
 
             if (tank.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Tank Brothers\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Tank Brothers_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Tank Brothers\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Tank Brothers_HaveDiscovered\": \"True\",");
             }
 
             if (laser.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Laser Tennis\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Laser Tennis_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Laser Tennis\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Laser Tennis_HaveDiscovered\": \"True\",");
             }
 
             if (space.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Low Gravity Room\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Low Gravity Room_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Low Gravity Room\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Low Gravity Room_HaveDiscovered\": \"True\",");
             }
 
             if (batting.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Baseball\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Baseball_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Baseball\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Baseball_HaveDiscovered\": \"True\",");
             }
 
             if (feed.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Throwing Hoops\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Throwing Hoops_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Throwing Hoops\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Throwing Hoops_HaveDiscovered\": \"True\",");
             }
 
             if (rodeo.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Rodeo\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Rodeo_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Rodeo\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Rodeo_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"MinigameCounter.Rodeo_CodyWinsData\": \"1\",");
             }
 
             if (birdstar.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Birdstar\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Birdstar_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Birdstar\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Birdstar_HaveDiscovered\": \"True\",");
             }
 
             if (bomb.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Bomb Run\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Bomb Run_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Bomb Run\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Bomb Run_HaveDiscovered\": \"True\",");
             }
 
             if (horse.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Horse Derby\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Horse Derby_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Horse Derby\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Horse Derby_HaveDiscovered\": \"True\",");
             }
 
             if (snow.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Snowball Warfare\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Snowball Warfare_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Snowball Warfare\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Snowball Warfare_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"MinigameCounter.Snowball Warfare_MayWinsData\": \"1\",");
             }
 
             if (shuffle.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Shuffle Board\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Shuffle Board_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Shuffle Board\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Shuffle Board_HaveDiscovered\": \"True\",");
             }
 
             if (icicle.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Icicle Throwing\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Icicle Throwing_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Icicle Throwing\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Icicle Throwing_HaveDiscovered\": \"True\",");
             }
 
             if (ice.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Ice Race\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Ice Race_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Ice Race\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Ice Race_HaveDiscovered\": \"True\",");
             }
 
             if (larva.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Larva Basket\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Larva Basket_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Larva Basket\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Larva Basket_HaveDiscovered\": \"True\",");
             }
 
             if (garden.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Garden Swing\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Garden Swing_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Garden Swing\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Garden Swing_HaveDiscovered\": \"True\",");
             }
 
             if (snail.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Snail Race\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Snail Race_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Snail Race\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Snail Race_HaveDiscovered\": \"True\",");
             }
 
             if (musical.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Musical Chairs\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Musical Chairs_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Musical Chairs\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Musical Chairs_HaveDiscovered\": \"True\",");
             }
 
             if (track.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Track Runner\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Track Runner_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Track Runner\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Track Runner_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"MinigameCounter.Track Runner_CodyWinsData\": \"1\",");
             }
 
             if (slotcars.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Slotcars\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Slotcars_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Slotcars\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Slotcars_HaveDiscovered\": \"True\",");
             }
 
             if (chess.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Chess\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Chess_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Chess\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Chess_HaveDiscovered\": \"True\",");
             }
 
             if (volleyball.IsChecked == true)
             {
-                saveLinesList.Add("	\"Minigame.Volleyball\": \"true\",");
-                saveLinesList.Add("	\"MinigameFlag.Volleyball_HaveDiscovered\": \"True\",");
+                saveLinesList.Add("\t\"Minigame.Volleyball\": \"true\",");
+                saveLinesList.Add("\t\"MinigameFlag.Volleyball_HaveDiscovered\": \"True\",");
             }
+        }
+
+        private void SelectAll_Checked(object sender, RoutedEventArgs e)
+        {
+            whack.IsChecked = flip.IsChecked = tug.IsChecked = plunger.IsChecked = tank.IsChecked = laser.IsChecked = space.IsChecked = 
+            batting.IsChecked = feed.IsChecked = rodeo.IsChecked = birdstar.IsChecked = bomb.IsChecked = horse.IsChecked = snow.IsChecked =
+            shuffle.IsChecked = icicle.IsChecked = ice.IsChecked = larva.IsChecked = garden.IsChecked = snail.IsChecked =
+            musical.IsChecked = track.IsChecked = slotcars.IsChecked = chess.IsChecked = volleyball.IsChecked = true;
+        }
+
+        private void SelectAll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            whack.IsChecked = flip.IsChecked = tug.IsChecked = plunger.IsChecked = tank.IsChecked = laser.IsChecked = space.IsChecked =
+            batting.IsChecked = feed.IsChecked = rodeo.IsChecked = birdstar.IsChecked = bomb.IsChecked = horse.IsChecked = snow.IsChecked =
+            shuffle.IsChecked = icicle.IsChecked = ice.IsChecked = larva.IsChecked = garden.IsChecked = snail.IsChecked =
+            musical.IsChecked = track.IsChecked = slotcars.IsChecked = chess.IsChecked = volleyball.IsChecked = false;
+        }
+
+        private void SelectAll_Indeterminate(object sender, RoutedEventArgs e)
+        {
+            if (whack.IsChecked == true &&
+                flip.IsChecked == true &&
+                tug.IsChecked == true &&
+                plunger.IsChecked == true &&
+                tank.IsChecked == true &&
+                laser.IsChecked == true &&
+                space.IsChecked == true &&
+                batting.IsChecked == true &&
+                feed.IsChecked == true &&
+                rodeo.IsChecked == true &&
+                birdstar.IsChecked == true &&
+                bomb.IsChecked == true &&
+                horse.IsChecked == true &&
+                snow.IsChecked == true &&
+                shuffle.IsChecked == true &&
+                icicle.IsChecked == true &&
+                ice.IsChecked == true &&
+                larva.IsChecked == true &&
+                garden.IsChecked == true &&
+                snail.IsChecked == true &&
+                musical.IsChecked == true &&
+                track.IsChecked == true &&
+                slotcars.IsChecked == true &&
+                chess.IsChecked == true &&
+                volleyball.IsChecked == true)
+            {
+                OptionsAllCheckBox.IsChecked = false;
+            }
+        }
+
+        private void SetCheckedState()
+        {
+            // Controls are null the first time this is called, so we just 
+            // need to perform a null check on any one of the controls.
+            if (volleyball != null)
+            {
+                if (whack.IsChecked == true &&
+                    flip.IsChecked == true &&
+                    tug.IsChecked == true &&
+                    plunger.IsChecked == true &&
+                    tank.IsChecked == true &&
+                    laser.IsChecked == true &&
+                    space.IsChecked == true &&
+                    batting.IsChecked == true &&
+                    feed.IsChecked == true &&
+                    rodeo.IsChecked == true &&
+                    birdstar.IsChecked == true &&
+                    bomb.IsChecked == true &&
+                    horse.IsChecked == true &&
+                    snow.IsChecked == true &&
+                    shuffle.IsChecked == true &&
+                    icicle.IsChecked == true &&
+                    ice.IsChecked == true &&
+                    larva.IsChecked == true &&
+                    garden.IsChecked == true &&
+                    snail.IsChecked == true &&
+                    musical.IsChecked == true &&
+                    track.IsChecked == true &&
+                    slotcars.IsChecked == true &&
+                    chess.IsChecked == true &&
+                    volleyball.IsChecked == true)
+                {
+                    OptionsAllCheckBox.IsChecked = true;
+                }
+                else if (whack.IsChecked == false &&
+                    flip.IsChecked == false &&
+                    tug.IsChecked == false &&
+                    plunger.IsChecked == false &&
+                    tank.IsChecked == false &&
+                    laser.IsChecked == false &&
+                    space.IsChecked == false &&
+                    batting.IsChecked == false &&
+                    feed.IsChecked == false &&
+                    rodeo.IsChecked == false &&
+                    birdstar.IsChecked == false &&
+                    bomb.IsChecked == false &&
+                    horse.IsChecked == false &&
+                    snow.IsChecked == false &&
+                    shuffle.IsChecked == false &&
+                    icicle.IsChecked == false &&
+                    ice.IsChecked == false &&
+                    larva.IsChecked == false &&
+                    garden.IsChecked == false &&
+                    snail.IsChecked == false &&
+                    musical.IsChecked == false &&
+                    track.IsChecked == false &&
+                    slotcars.IsChecked == false &&
+                    chess.IsChecked == false &&
+                    volleyball.IsChecked == false)
+                {
+                    OptionsAllCheckBox.IsChecked = false;
+                }
+                else
+                {
+                    // Set third state (indeterminate) by setting IsChecked to null.
+                    OptionsAllCheckBox.IsChecked = null;
+                }
+            }
+        }
+
+        private void Option_Checked(object sender, RoutedEventArgs e)
+        {
+            SetCheckedState();
+        }
+
+        private void Option_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SetCheckedState();
+        }
+
+        private void AnyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OptionsAllCheckBox.IsChecked = false;
+            tug.IsChecked = true;
+            plunger.IsChecked = true;
+            batting.IsChecked = true;
+            snail.IsChecked = true;
+            track.IsChecked = true;
         }
     }
 }
